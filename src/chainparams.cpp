20ed0ee2b35961f4638080b0372eedf09a0ee752
Suhas Daftuar <sdaftuar@gmail.com>
2016-07-25 17:22:37 -0400

    Allow changing BIP9 parameters on regtest

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1012,6 +1012,12 @@ public:
         // BIP44 test coin type is '1' (All coin's testnet default)
         nLegacyExtCoinType = 1;
    }
+
+    void UpdateBIP9Parameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout)
+    {
+        consensus.vDeployments[d].nStartTime = nStartTime;
+        consensus.vDeployments[d].nTimeout = nTimeout;
+    }
 };
 static CRegTestParams regTestParams;
 
@@ -1049,4 +1055,9 @@ void SelectParams(const std::string& network)
 
     SelectBaseParams(network);
     pCurrentParams = &Params(network);
+}
+
+void UpdateRegtestBIP9Parameters(Consensus::DeploymentPos d, int64_t nStartTime, int64_t nTimeout)
+{
+    regTestParams.UpdateBIP9Parameters(d, nStartTime, nTimeout);
 }
