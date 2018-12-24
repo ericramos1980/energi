bytzck/BYTZ

Merge #8811: rpc: Add support for JSON-RPC named arguments
4e7e2e1 Update RPC argument names (John Newbery)
481f289 rpc: Named argument support for bitcoin-cli (Wladimir J. van der Laan)
9adb4e1 rpc: Argument name consistency (Wladimir J. van der Laan)
8d713f7 rpc: Named arguments for rawtransaction calls (Wladimir J. van der Laan)
37a166f rpc: Named arguments for wallet calls (Wladimir J. van der Laan)
78b684f rpc: Named arguments for mining calls (Wladimir J. van der Laan)
b8ebc59 rpc: Named arguments for net calls (Wladimir J. van der Laan)
2ca9dcd test: Add test for RPC named arguments (Wladimir J. van der Laan)
fba1a61 rpc: Named arguments for misc calls (Wladimir J. van der Laan)
286ec08 rpc: Add 'echo' call for testing (Wladimir J. van der Laan)
495eb44 rpc: Named arguments for blockchain calls (Wladimir J. van der Laan)
6f1c76a rpc: Support named arguments (Wladimir J. van der Laan)
5865d41 authproxy: Add support for RPC named arguments (Wladimir J. van der Laan)

src/rpc/governance.cpp
@@ -997,12 +997,13 @@ UniValue getsuperblockbudget(const JSONRPCRequest& request)
 }
 
 static const CRPCCommand commands[] =
-{ //  category              name                      actor (function)         okSafeMode
+{ //  category              name                      actor (function)         okSafe argNames
+  //  --------------------- ------------------------  -----------------------  ------ ----------
     /* Dash features */
-    { "dash",               "getgovernanceinfo",      &getgovernanceinfo,      true  },
-    { "dash",               "getsuperblockbudget",    &getsuperblockbudget,    true  },
-    { "dash",               "gobject",                &gobject,                true  },
-    { "dash",               "voteraw",                &voteraw,                true  },
+    { "dash",               "getgovernanceinfo",      &getgovernanceinfo,      true,  {} },
+    { "dash",               "getsuperblockbudget",    &getsuperblockbudget,    true,  {"index"} },
+    { "dash",               "gobject",                &gobject,                true,  {} },
+    { "dash",               "voteraw",                &voteraw,                true,  {} },
 
 };
 

src/rpc/masternode.cpp
@@ -854,15 +854,15 @@ UniValue sentinelping(const JSONRPCRequest& request)
 }
 
 static const CRPCCommand commands[] =
-{ //  category              name                      actor (function)         okSafeMode
-    /* Dash features */
-    { "dash",               "masternode",             &masternode,             true  },
-    { "dash",               "masternodelist",         &masternodelist,         true  },
-    { "dash",               "masternodebroadcast",    &masternodebroadcast,    true  },
-    { "dash",               "getpoolinfo",            &getpoolinfo,            true  },
-    { "dash",               "sentinelping",           &sentinelping,           true  },
+{ //  category              name                      actor (function)         okSafe argNames
+  //  --------------------- ------------------------  -----------------------  ------ ----------
+    { "dash",               "masternode",             &masternode,             true,  {} },
+    { "dash",               "masternodelist",         &masternodelist,         true,  {} },
+    { "dash",               "masternodebroadcast",    &masternodebroadcast,    true,  {} },
+    { "dash",               "getpoolinfo",            &getpoolinfo,            true,  {} },
+    { "dash",               "sentinelping",           &sentinelping,           true,  {} },
 #ifdef ENABLE_WALLET
-    { "dash",               "privatesend",            &privatesend,            false },
+    { "dash",               "privatesend",            &privatesend,            false, {} },
 #endif // ENABLE_WALLET
 };
 

src/rpc/misc.cpp
@@ -281,14 +281,14 @@ UniValue validateaddress(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 1)
         throw runtime_error(
-            "validateaddress \"dashaddress\"\n"
+            "validateaddress \"address\"\n"
             "\nReturn information about the given dash address.\n"
             "\nArguments:\n"
-            "1. \"dashaddress\"     (string, required) The dash address to validate\n"
+            "1. \"address\"     (string, required) The dash address to validate\n"
             "\nResult:\n"
             "{\n"
             "  \"isvalid\" : true|false,       (boolean) If the address is valid or not. If not, this is the only property returned.\n"
-            "  \"address\" : \"dashaddress\", (string) The dash address validated\n"
+            "  \"address\" : \"address\", (string) The dash address validated\n"
             "  \"scriptPubKey\" : \"hex\",       (string) The hex encoded scriptPubKey generated by the address\n"
             "  \"ismine\" : true|false,        (boolean) If the address is yours or not\n"
             "  \"iswatchonly\" : true|false,   (boolean) If the address is watchonly\n"
@@ -455,10 +455,10 @@ UniValue verifymessage(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 3)
         throw runtime_error(
-            "verifymessage \"dashaddress\" \"signature\" \"message\"\n"
+            "verifymessage \"address\" \"signature\" \"message\"\n"
             "\nVerify a signed message\n"
             "\nArguments:\n"
-            "1. \"dashaddress\"  (string, required) The dash address to use for the signature.\n"
+            "1. \"address\"         (string, required) The dash address to use for the signature.\n"
             "2. \"signature\"       (string, required) The signature provided by the signer in base 64 encoding (see signmessage).\n"
             "3. \"message\"         (string, required) The message that was signed.\n"
             "\nResult:\n"
@@ -1086,31 +1086,46 @@ UniValue getmemoryinfo(const JSONRPCRequest& request)
     return obj;
 }
 
+UniValue echo(const JSONRPCRequest& request)
+{
+    if (request.fHelp)
+        throw runtime_error(
+            "echo|echojson \"message\" ...\n"
+            "\nSimply echo back the input arguments. This command is for testing.\n"
+            "\nThe difference between echo and echojson is that echojson has argument conversion enabled in the client-side table in"
+            "bitcoin-cli and the GUI. There is no server-side difference."
+        );
+
+    return request.params;
+}
+
 static const CRPCCommand commands[] =
 { //  category              name                      actor (function)         okSafeMode
   //  --------------------- ------------------------  -----------------------  ----------
-    { "control",            "debug",                  &debug,                  true  },
-    { "control",            "getinfo",                &getinfo,                true  }, /* uses wallet if enabled */
-    { "control",            "getmemoryinfo",          &getmemoryinfo,          true  },
-    { "util",               "validateaddress",        &validateaddress,        true  }, /* uses wallet if enabled */
-    { "util",               "createmultisig",         &createmultisig,         true  },
-    { "util",               "verifymessage",          &verifymessage,          true  },
-    { "util",               "signmessagewithprivkey", &signmessagewithprivkey, true  },
-    { "blockchain",         "getspentinfo",           &getspentinfo,           false },
+    { "control",            "debug",                  &debug,                  true,  {} },
+    { "control",            "getinfo",                &getinfo,                true,  {} }, /* uses wallet if enabled */
+    { "control",            "getmemoryinfo",          &getmemoryinfo,          true,  {} },
+    { "util",               "validateaddress",        &validateaddress,        true,  {"address"} }, /* uses wallet if enabled */
+    { "util",               "createmultisig",         &createmultisig,         true,  {"nrequired","keys"} },
+    { "util",               "verifymessage",          &verifymessage,          true,  {"address","signature","message"} },
+    { "util",               "signmessagewithprivkey", &signmessagewithprivkey, true,  {"privkey","message"} },
+    { "blockchain",         "getspentinfo",           &getspentinfo,           false, {"json"} },
 
     /* Address index */
-    { "addressindex",       "getaddressmempool",      &getaddressmempool,      true  },
-    { "addressindex",       "getaddressutxos",        &getaddressutxos,        false },
-    { "addressindex",       "getaddressdeltas",       &getaddressdeltas,       false },
-    { "addressindex",       "getaddresstxids",        &getaddresstxids,        false },
-    { "addressindex",       "getaddressbalance",      &getaddressbalance,      false },
+    { "addressindex",       "getaddressmempool",      &getaddressmempool,      true,  {"addresses"}  },
+    { "addressindex",       "getaddressutxos",        &getaddressutxos,        false, {"addresses"} },
+    { "addressindex",       "getaddressdeltas",       &getaddressdeltas,       false, {"addresses"} },
+    { "addressindex",       "getaddresstxids",        &getaddresstxids,        false, {"addresses"} },
+    { "addressindex",       "getaddressbalance",      &getaddressbalance,      false, {"addresses"} },
 
     /* Dash features */
-    { "dash",               "mnsync",                 &mnsync,                 true  },
-    { "dash",               "spork",                  &spork,                  true  },
+    { "dash",               "mnsync",                 &mnsync,                 true,  {} },
+    { "dash",               "spork",                  &spork,                  true,  {"datetime"} },
 
     /* Not shown in help */
-    { "hidden",             "setmocktime",            &setmocktime,            true  },
+    { "hidden",             "setmocktime",            &setmocktime,            true,  {"timestamp"}},
+    { "hidden",             "echo",                   &echo,                   true,  {"arg0","arg1","arg2","arg3","arg4","arg5","arg6","arg7","arg8","arg9"}},
+    { "hidden",             "echojson",               &echo,                  true,  {"arg0","arg1","arg2","arg3","arg4","arg5","arg6","arg7","arg8","arg9"}},
 };
 
 void RegisterMiscRPCCommands(CRPCTable &t)

src/wallet/rpcdump.cpp
@@ -83,7 +83,7 @@ UniValue importprivkey(const JSONRPCRequest& request)
     
     if (request.fHelp || request.params.size() < 1 || request.params.size() > 3)
         throw runtime_error(
-            "importprivkey \"dashprivkey\" ( \"label\" rescan )\n"
+            "importprivkey \"dashprivkey\" ( \"label\" ) ( rescan )\n"
             "\nAdds a private key (as returned by dumpprivkey) to your wallet.\n"
             "\nArguments:\n"
             "1. \"dashprivkey\"   (string, required) The private key (see dumpprivkey)\n"
@@ -655,11 +655,11 @@ UniValue dumpprivkey(const JSONRPCRequest& request)
     
     if (request.fHelp || request.params.size() != 1)
         throw runtime_error(
-            "dumpprivkey \"dashaddress\"\n"
-            "\nReveals the private key corresponding to 'dashaddress'.\n"
+            "dumpprivkey \"address\"\n"
+            "\nReveals the private key corresponding to 'address'.\n"
             "Then the importprivkey can be used with this output\n"
             "\nArguments:\n"
-            "1. \"dashaddress\"   (string, required) The dash address for the private key\n"
+            "1. \"address\"   (string, required) The bitcoin address for the private key\n"
             "\nResult:\n"
             "\"key\"                (string) The private key\n"
             "\nExamples:\n"
@@ -1167,10 +1167,10 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
     // clang-format off
     if (mainRequest.fHelp || mainRequest.params.size() < 1 || mainRequest.params.size() > 2)
         throw runtime_error(
-            "importmulti '[<json import requests>]' '<json options>' \n\n"
+            "importmulti \"requests\" \"options\"\n\n"
             "Import addresses/scripts (with private or public keys, redeem script (P2SH)), rescanning all addresses in one-shot-only (rescan can be disabled via options).\n\n"
             "Arguments:\n"
-            "1. request array     (array, required) Data to be imported\n"
+            "1. requests     (array, required) Data to be imported\n"
             "  [     (array of json objects)\n"
             "    {\n"
             "      \"scriptPubKey\": \"<script>\" | { \"address\":\"<address>\" }, (string / json, required) Type of scriptPubKey (string for script, json for address)\n"
@@ -1184,7 +1184,7 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
             "    }\n"
             "  ,...\n"
             "  ]\n"
-            "2. json options                 (json, optional)\n"
+            "2. options                 (json, optional)\n"
             "  {\n"
             "     \"rescan\": <false>,         (boolean, optional, default: true) Stating if should rescan the blockchain after all imports\n"
             "  }\n"

ericramos1980/energi

src/rpc/governance.cpp
@@ -983,12 +983,13 @@ UniValue getsuperblockbudget(const JSONRPCRequest& request)
 }
 
 static const CRPCCommand commands[] =
-{ //  category              name                      actor (function)         okSafeMode
+{ //  category              name                      actor (function)         okSafe argNames
+  //  --------------------- ------------------------  -----------------------  ------ ----------
     /* Energi features */
-    { "energi",             "getgovernanceinfo",      &getgovernanceinfo,      true  },
-    { "energi",             "getsuperblockbudget",    &getsuperblockbudget,    true  },
-    { "energi",             "gobject",                &gobject,                true  },
-    { "energi",             "voteraw",                &voteraw,                true  },
+    { "energi",             "getgovernanceinfo",      &getgovernanceinfo,      true,  {} },
+    { "energi",             "getsuperblockbudget",    &getsuperblockbudget,    true,  {"index"} },
+    { "energi",             "gobject",                &gobject,                true,  {} },
+    { "energi",             "voteraw",                &voteraw,                true,  {} },
 
 };
 

src/rpc/masternode.cpp
@@ -870,15 +870,15 @@ UniValue sentinelping(const JSONRPCRequest& request)
 }
 
 static const CRPCCommand commands[] =
-{ //  category              name                      actor (function)         okSafeMode
-    /* Dash features */
-    { "energi",             "masternode",             &masternode,             true  },
-    { "energi",             "masternodelist",         &masternodelist,         true  },
-    { "energi",             "masternodebroadcast",    &masternodebroadcast,    true  },
-    { "energi",             "getpoolinfo",            &getpoolinfo,            true  },
-    { "energi",             "sentinelping",           &sentinelping,           true  },
+{ //  category              name                      actor (function)         okSafe argNames
+  //  --------------------- ------------------------  -----------------------  ------ ----------
+    { "energi",             "masternode",             &masternode,             true,  {} },
+    { "energi",             "masternodelist",         &masternodelist,         true,  {} },
+    { "energi",             "masternodebroadcast",    &masternodebroadcast,    true,  {} },
+    { "energi",             "getpoolinfo",            &getpoolinfo,            true,  {} },
+    { "energi",             "sentinelping",           &sentinelping,           true,  {} },
 #ifdef ENABLE_WALLET
-    { "energi",             "privatesend",            &privatesend,            false },
+    { "energi",             "privatesend",            &privatesend,            false, {} },
 #endif // ENABLE_WALLET
 };
 

src/rpc/misc.cpp
@@ -281,14 +281,14 @@ UniValue validateaddress(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 1)
         throw runtime_error(
-            "validateaddress \"energiaddress\"\n"
+            "validateaddress \"address\"\n"
             "\nReturn information about the given energi address.\n"
             "\nArguments:\n"
-            "1. \"energiaddress\"     (string, required) The energi address to validate\n"
+            "1. \"address\"     (string, required) The energi address to validate\n"
             "\nResult:\n"
             "{\n"
             "  \"isvalid\" : true|false,       (boolean) If the address is valid or not. If not, this is the only property returned.\n"
-            "  \"address\" : \"energiaddress\", (string) The energi address validated\n"
+            "  \"address\" : \"address\", (string) The energi address validated\n"
             "  \"scriptPubKey\" : \"hex\",       (string) The hex encoded scriptPubKey generated by the address\n"
             "  \"ismine\" : true|false,        (boolean) If the address is yours or not\n"
             "  \"iswatchonly\" : true|false,   (boolean) If the address is watchonly\n"
@@ -455,10 +455,10 @@ UniValue verifymessage(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 3)
         throw runtime_error(
-            "verifymessage \"energiaddress\" \"signature\" \"message\"\n"
+            "verifymessage \"address\" \"signature\" \"message\"\n"
             "\nVerify a signed message\n"
             "\nArguments:\n"
-            "1. \"energiaddress\"  (string, required) The energi address to use for the signature.\n"
+            "1. \"address\"         (string, required) The energi address to use for the signature.\n"
             "2. \"signature\"       (string, required) The signature provided by the signer in base 64 encoding (see signmessage).\n"
             "3. \"message\"         (string, required) The message that was signed.\n"
             "\nResult:\n"
@@ -1086,31 +1086,46 @@ UniValue getmemoryinfo(const JSONRPCRequest& request)
     return obj;
 }
 
+UniValue echo(const JSONRPCRequest& request)
+{
+    if (request.fHelp)
+        throw runtime_error(
+            "echo|echojson \"message\" ...\n"
+            "\nSimply echo back the input arguments. This command is for testing.\n"
+            "\nThe difference between echo and echojson is that echojson has argument conversion enabled in the client-side table in"
+            "bitcoin-cli and the GUI. There is no server-side difference."
+        );
+
+    return request.params;
+}
+
 static const CRPCCommand commands[] =
 { //  category              name                      actor (function)         okSafeMode
   //  --------------------- ------------------------  -----------------------  ----------
-    { "control",            "debug",                  &debug,                  true  },
-    { "control",            "getinfo",                &getinfo,                true  }, /* uses wallet if enabled */
-    { "control",            "getmemoryinfo",          &getmemoryinfo,          true  },
-    { "util",               "validateaddress",        &validateaddress,        true  }, /* uses wallet if enabled */
-    { "util",               "createmultisig",         &createmultisig,         true  },
-    { "util",               "verifymessage",          &verifymessage,          true  },
-    { "util",               "signmessagewithprivkey", &signmessagewithprivkey, true  },
-    { "blockchain",         "getspentinfo",           &getspentinfo,           false },
+    { "control",            "debug",                  &debug,                  true,  {} },
+    { "control",            "getinfo",                &getinfo,                true,  {} }, /* uses wallet if enabled */
+    { "control",            "getmemoryinfo",          &getmemoryinfo,          true,  {} },
+    { "util",               "validateaddress",        &validateaddress,        true,  {"address"} }, /* uses wallet if enabled */
+    { "util",               "createmultisig",         &createmultisig,         true,  {"nrequired","keys"} },
+    { "util",               "verifymessage",          &verifymessage,          true,  {"address","signature","message"} },
+    { "util",               "signmessagewithprivkey", &signmessagewithprivkey, true,  {"privkey","message"} },
+    { "blockchain",         "getspentinfo",           &getspentinfo,           false, {"json"} },
 
     /* Address index */
-    { "addressindex",       "getaddressmempool",      &getaddressmempool,      true  },
-    { "addressindex",       "getaddressutxos",        &getaddressutxos,        false },
-    { "addressindex",       "getaddressdeltas",       &getaddressdeltas,       false },
-    { "addressindex",       "getaddresstxids",        &getaddresstxids,        false },
-    { "addressindex",       "getaddressbalance",      &getaddressbalance,      false },
+    { "addressindex",       "getaddressmempool",      &getaddressmempool,      true,  {"addresses"}  },
+    { "addressindex",       "getaddressutxos",        &getaddressutxos,        false, {"addresses"} },
+    { "addressindex",       "getaddressdeltas",       &getaddressdeltas,       false, {"addresses"} },
+    { "addressindex",       "getaddresstxids",        &getaddresstxids,        false, {"addresses"} },
+    { "addressindex",       "getaddressbalance",      &getaddressbalance,      false, {"addresses"} },
 
-    /* Dash features */
-    { "energi",             "mnsync",                 &mnsync,                 true  },
-    { "energi",             "spork",                  &spork,                  true  },
+    /* Energi features */
+    { "energi",             "mnsync",                 &mnsync,                 true,  {} },
+    { "energi",             "spork",                  &spork,                  true,  {"datetime"} },
 
     /* Not shown in help */
-    { "hidden",             "setmocktime",            &setmocktime,            true  },
+    { "hidden",             "setmocktime",            &setmocktime,            true,  {"timestamp"}},
+    { "hidden",             "echo",                   &echo,                   true,  {"arg0","arg1","arg2","arg3","arg4","arg5","arg6","arg7","arg8","arg9"}},
+    { "hidden",             "echojson",               &echo,                  true,  {"arg0","arg1","arg2","arg3","arg4","arg5","arg6","arg7","arg8","arg9"}},
 };
 
 void RegisterMiscRPCCommands(CRPCTable &t)

src/wallet/rpcdump.cpp
@@ -83,7 +83,7 @@ UniValue importprivkey(const JSONRPCRequest& request)
     
     if (request.fHelp || request.params.size() < 1 || request.params.size() > 3)
         throw runtime_error(
-            "importprivkey \"energiprivkey\" ( \"label\" rescan )\n"
+            "importprivkey \"energiprivkey\" ( \"label\" ) ( rescan )\n"
             "\nAdds a private key (as returned by dumpprivkey) to your wallet.\n"
             "\nArguments:\n"
             "1. \"energiprivkey\"   (string, required) The private key (see dumpprivkey)\n"
@@ -655,12 +655,12 @@ UniValue dumpprivkey(const JSONRPCRequest& request)
     
     if (request.fHelp || request.params.size() != 2 || !RPCCheckAuthCode(request.params[1]))
         throw runtime_error(
-            "dumpprivkey \"energiaddress\" \"one_time_code\"\n"
+            "dumpprivkey \"address\" \"one_time_code\"\n"
             + RPCGetAuthCodeHelp() +
             "\nReveals the private key corresponding to 'energiaddress'.\n"
             "Then the importprivkey can be used with this output\n"
             "\nArguments:\n"
-            "1. \"energiaddress\"   (string, required) The energi address for the private key\n"
+            "1. \"address\"         (string, required) The energi address for the private key\n"
             "2. \"one_time_code\"   (string, required) The security code from the previous invocation\n"
             "\nResult:\n"
             "\"key\"                (string) The private key\n"
@@ -1174,10 +1174,10 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
     // clang-format off
     if (mainRequest.fHelp || mainRequest.params.size() < 1 || mainRequest.params.size() > 2)
         throw runtime_error(
-            "importmulti '[<json import requests>]' '<json options>' \n\n"
+            "importmulti \"requests\" \"options\"\n\n"
             "Import addresses/scripts (with private or public keys, redeem script (P2SH)), rescanning all addresses in one-shot-only (rescan can be disabled via options).\n\n"
             "Arguments:\n"
-            "1. request array     (array, required) Data to be imported\n"
+            "1. requests     (array, required) Data to be imported\n"
             "  [     (array of json objects)\n"
             "    {\n"
             "      \"scriptPubKey\": \"<script>\" | { \"address\":\"<address>\" }, (string / json, required) Type of scriptPubKey (string for script, json for address)\n"
@@ -1191,7 +1191,7 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
             "    }\n"
             "  ,...\n"
             "  ]\n"
-            "2. json options                 (json, optional)\n"
+            "2. options                 (json, optional)\n"
             "  {\n"
             "     \"rescan\": <false>,         (boolean, optional, default: true) Stating if should rescan the blockchain after all imports\n"
             "  }\n"