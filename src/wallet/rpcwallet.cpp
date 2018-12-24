bytzck/BYTZ

Move privatesend to rpcwallet.cpp as well after Bitcoin #7307 was backported
Allows to compile with no wallet again.

src/wallet/rpcwallet.cpp
@@ -2778,6 +2778,8 @@ const CRPCCommand vWalletRPCCommands[] =
     { "wallet",             "instantsendtoaddress",     &instantsendtoaddress,     false },
     { "wallet",             "dumphdinfo",               &dumphdinfo,               true  },
     { "wallet",             "importelectrumwallet",     &importelectrumwallet,     true  },
+
+    { "dash",               "privatesend",              &privatesend,              false },
 };
 
 void walletRegisterRPCCommands()

ericramos1980/energi

src/wallet/rpcwallet.cpp
@@ -2778,6 +2778,8 @@ const CRPCCommand vWalletRPCCommands[] =
     { "wallet",             "instantsendtoaddress",     &instantsendtoaddress,     false },
     { "wallet",             "dumphdinfo",               &dumphdinfo,               true  },
     { "wallet",             "importelectrumwallet",     &importelectrumwallet,     true  },
+
+    { "energi",             "privatesend",              &privatesend,              false },
 };
 
 void walletRegisterRPCCommands()