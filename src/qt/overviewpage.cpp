bytzck/BYTZ

Merge #8793: Do not shadow in src/qt
f839350 Do not shadow in src/qt (Pavel Janík)

src/qt/overviewpage.cpp
@@ -38,7 +38,7 @@ class TxViewDelegate : public QAbstractItemDelegate
     Q_OBJECT
 public:
     TxViewDelegate(const PlatformStyle *_platformStyle, QObject *parent=nullptr):
-        QAbstractItemDelegate(parent), unit(BitcoinUnits::DASH),
+        QAbstractItemDelegate(), unit(BitcoinUnits::DASH),
         platformStyle(_platformStyle)
     {
 

ericramos1980/energi

src/qt/overviewpage.cpp
@@ -38,7 +38,7 @@ class TxViewDelegate : public QAbstractItemDelegate
     Q_OBJECT
 public:
     TxViewDelegate(const PlatformStyle *_platformStyle, QObject *parent=nullptr):
-        QAbstractItemDelegate(parent), unit(BitcoinUnits::NRG),
+        QAbstractItemDelegate(), unit(BitcoinUnits::NRG),
         platformStyle(_platformStyle)
     {
 