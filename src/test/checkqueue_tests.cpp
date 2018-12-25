bytzck/BYTZ

Include test_dash.h instead of test_bitcoin.h

src/test/checkqueue_tests.cpp
@@ -6,7 +6,7 @@
 #include "utiltime.h"
 #include "validation.h"
 
-#include "test/test_bitcoin.h"
+#include "test/test_dash.h"
 #include "checkqueue.h"
 #include <boost/test/unit_test.hpp>
 #include <boost/thread.hpp>

src/wallet/test/wallet_tests.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 
 #include "rpc/server.h"
-#include "test/test_bitcoin.h"
+#include "test/test_dash.h"
 #include "validation.h"
 #include "wallet/test/wallet_test_fixture.h"
 

ericramos1980/energi

src/test/checkqueue_tests.cpp
@@ -6,7 +6,7 @@
 #include "utiltime.h"
 #include "validation.h"
 
-#include "test/test_bitcoin.h"
+#include "test/test_energi.h"
 #include "checkqueue.h"
 #include <boost/test/unit_test.hpp>
 #include <boost/thread.hpp>

src/wallet/test/wallet_tests.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 
 #include "rpc/server.h"
-#include "test/test_bitcoin.h"
+#include "test/test_energi.h"
 #include "validation.h"
 #include "wallet/test/wallet_test_fixture.h"
 