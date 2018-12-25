tmagik/catoshi

Merge #9516: Bug-fix: listsinceblock: use fork point as reference for blocks in reorg'd chains
7ba0a00 Testing: listsinceblock should not use orphan block height. (Karl-Johan Alm)
ee5c1ce Bug-fix: listsinceblock: use closest common ancestor when a block hash was provided for a chain that was not the main chain. (Karl-Johan Alm)

qa/pull-tester/rpc-tests.py
@@ -151,6 +151,7 @@ testScripts = [
     'signmessages.py',
     'nulldummy.py',
     'import-rescan.py',
+    'bumpfee.py',
     'rpcnamedargs.py',
     'listsinceblock.py',
 ]

ericramos1980/energi

qa/pull-tester/rpc-tests.py
@@ -172,6 +172,7 @@ testScripts = [
     'signmessages.py',
     'import-rescan.py',
     'rpcnamedargs.py',
+    'listsinceblock.py',
 ]
 if ENABLE_ZMQ:
     testScripts.append('zmq_test.py')