7e96b9d097ace49d203d81d39f28ba0f65e40642
Andrey Galkin <andrey@futoin.org>
2019-01-03 06:42:03 +0200

    FIXED: splashscreen text positioning

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -39,9 +39,9 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
 
     // set reference point, paddings
     int paddingLeft             = 14;
-    int paddingTop              = 470;
-    int titleVersionVSpace      = 17;
-    int titleCopyrightVSpace    = 22;
+    int paddingTop              = 410;
+    int titleVersionVSpace      = 25;
+    int titleCopyrightVSpace    = 32;
 
     float fontFactor            = 1.0;
 
