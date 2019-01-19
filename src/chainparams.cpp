190a7738530432867cc6e7e878c868efcdd0e85c
Andrey Galkin <andrey@futoin.org>
2019-01-19 03:16:05 +0200

    NEW: checkpoint to the last superblock at 383040

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -338,7 +338,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_BIP147].nThreshold = 3226; // 80% of 4032
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000001482d9b970cbed2c");
+        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000191ef2b777df7762");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x7bfb0cb837ab87389241dc9bb4c8881fde6ec7bdfe5f39d41bf6c886733cc8cb");
@@ -428,13 +428,15 @@ public:
             ( 310000, uint256S("0xca5cf4e91d19f5258326f677f4c53ac87d799bd7208a8cc9183438f8a7baa573"))
             ( 343000, uint256S("0x8018bb616584d31bb568a5867baf06742a99bdb1b55c541364a0587f07779036"))
             ( 360000, uint256S("0x7bfb0cb837ab87389241dc9bb4c8881fde6ec7bdfe5f39d41bf6c886733cc8cb"))
+            ( 370000, uint256S("0x461c0314744dc87237719076f281b7be1e724b77563d656267317cd727f781c7"))
+            ( 383040, uint256S("0x9d662c1e343b626f43b431ffdda02378b47d4dc34967d73102330650a493ad3c"))
      };
 
         chainTxData = ChainTxData{
-            1546390786,     // * UNIX timestamp of last known number of transactions
-            460784,         // * total number of transactions between genesis and that timestamp
+            1547844586,     // * UNIX timestamp of last known number of transactions
+            501412,         // * total number of transactions between genesis and that timestamp
                             //   (the tx=... number in the SetBestChain debug.log lines)
-            0.03          // * estimated number of transactions per second after that timestamp
+            0.028           // * estimated number of transactions per second after that timestamp
         };
     }
 };
